fa4dd11dbbeba27de9d6be58387453ac03d01980
Jorge Fabregat López <pilonsi@enganyapastors.local>
2022-07-12 23:04:01 +0200

    Initial project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+ # AEMET OpenData Analysis Tool
+
+Work in progress tool to compile and analyze climate data from AEMET's OpenData API.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clima3',
+      version='0.0',
+      description='AEMET OpenData Analyzer Tool',
+      author='Jorge Fabregat López',
+      author_email='jfabregat@protonmail.com',
+      url='https://github.com/pilonsi/clima3',
+      packages=['clima3'],
+      install_requires=[]
+)
